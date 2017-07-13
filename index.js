8e5364b5b0597109f3b09e8e178ac8a4eefac6c3
Sam Meech-Ward <sam@meech-ward.com>
2017-07-13 10:29:29 -0600

    Add bookmarks

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ app.get("/", (req, res) => {
   res.end("Hello iOS Developers!");
 });
 
+const bookmarks = {
+  compass: "http://ios-compass.lighthouselabs.ca/setup",
+  swift: "https://developer.apple.com/library/content/documentation/Swift/Conceptual/Swift_Programming_Language/TheBasics.html#//apple_ref/doc/uid/TP40014097-CH5-ID309"
+};
+
+app.get('/bookmarks', (req, res) => {
+  res.json(bookmarks);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
 });
